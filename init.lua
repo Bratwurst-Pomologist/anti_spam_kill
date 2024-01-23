185a667f6226bb7a24996bc603bc8c3c8b7f5364
2BW <71567744+Bratwurst-Pomologist@users.noreply.github.com>
2024-01-23 20:41:27 +0100

    update init.lua

--- init.lua
+++ init.lua
@@ -1,1 +1,10 @@
-
+core.register_on_dieplayer(function(player, reason)
+	local victim = player:get_player_name()
+	if reason.type == "punch" then
+		local obj = reason.object
+		if obj:is_player() then
+			local killer = obj:get_player_name()
+			--hud_add(killer,weap_tex.."^[resize:16x16",name)
+		end
+	end
+end)
